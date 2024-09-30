3adea1a6e234a6cec85687f4444a58256d3e307a
Alfarion <Gerontieff@mail.ru>
2024-09-30 15:05:11 +0300

    refactor button classes isolate

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
-import './Button.css'
+import classes from './Button.module.css'
 
 export default function Button({children,onClick, isActive}) {
 
     return (
-        <button className={`button ${isActive?'active':''}`} onClick={onClick}> {children} </button>
+        <button className={`${classes.button} ${isActive?`${classes.active}`:''}`} onClick={onClick}> {children} </button>
     )
 }
